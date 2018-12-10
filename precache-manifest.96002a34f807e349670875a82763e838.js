01de118c22c7c54bce54fd8e096ec5a21b272ebe
Alyssa W <awilliams62090@gmail.com>
2018-12-09 21:51:32 -0700

    Updates

---
+++ precache-manifest.96002a34f807e349670875a82763e838.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "426c4cb3fc0a1818757c",
+    "url": "/clicky-game/static/js/main.426c4cb3.chunk.js"
+  },
+  {
+    "revision": "66bf40fd69baa7384eae",
+    "url": "/clicky-game/static/js/1.66bf40fd.chunk.js"
+  },
+  {
+    "revision": "426c4cb3fc0a1818757c",
+    "url": "/clicky-game/static/css/main.7458c28d.chunk.css"
+  },
+  {
+    "revision": "66bf40fd69baa7384eae",
+    "url": "/clicky-game/static/css/1.5c33b1bf.chunk.css"
+  },
+  {
+    "revision": "afed5a6ffc966ad96239d5821c4e996d",
+    "url": "/clicky-game/index.html"
+  }
+];
